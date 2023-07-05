da2e87fd715233c8d2901100cfa9c0ae344ec63a
Mobin-Pourabedini <realmobinpourabedini@gmail.com>
2023-07-05 18:55:46 +0330

    add inital files

---
+++ src/YourConvertor.java
@@ -0,0 +1,11 @@
+public class YourConvertor{
+    public Object deserialize(String input, String className) {
+        //TODO Implement this method
+        return null;
+    }
+
+    public String serialize(Object input) {
+        //TODO Implement this method
+        return null;
+    }
+}
